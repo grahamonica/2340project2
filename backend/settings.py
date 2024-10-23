7ced163a56098f161b0eb78aa50b02c8844edb0d
Monica Graham <monicagraham40@gmail.com>
2024-10-23 16:38:52 -0400

    created home page

--- backend/settings.py
+++ backend/settings.py
@@ -121,3 +121,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# settings.py
+# settings.py
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # Add your template directories here
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',  # Required for admin navigation
+                'django.contrib.auth.context_processors.auth',  # Required for admin authentication
+                'django.contrib.messages.context_processors.messages',  # Required for admin messages
+                # Add any other context processors here
+            ],
+        },
+    },
+]
